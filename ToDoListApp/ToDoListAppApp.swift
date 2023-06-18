931781b690ea85d7a5145611a9862945fc87bb96
sirzes02 <sirzes02@gmail.com>
2023-06-18 14:01:25 -0500

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Santiago Varela on 18/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

2024596c36f876d2686ac0f2b1bfe4c55aa55042
sirzes02 <sirzes02@gmail.com>
2023-06-21 20:24:57 -0500

    implement request to change check values

--- ToDoListApp/ViewModels/ToDoListItemViewViewModel.swift
+++ ToDoListApp/ViewModels/ToDoListItemViewViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by Santiago Varela on 18/06/23.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 /// ViewModel for single to do list items view (each row in items list)
@@ -12,6 +14,18 @@ class ToDoListItemViewViewModel: ObservableObject {
     init() {}
     
     func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
         
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
     }
 }

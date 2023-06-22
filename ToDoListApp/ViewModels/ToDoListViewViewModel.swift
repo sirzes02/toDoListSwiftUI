3922ea89cc6da06ef0db3c177e191b146c5c2f45
sirzes02 <sirzes02@gmail.com>
2023-06-21 20:20:05 -0500

    implement request to delete todo

--- ToDoListApp/ViewModels/ToDoListViewViewModel.swift
+++ ToDoListApp/ViewModels/ToDoListViewViewModel.swift
@@ -5,14 +5,27 @@
 //  Created by Santiago Varela on 18/06/23.
 //
 
+import FirebaseFirestore
 import Foundation
 
 class ToDoListViewViewModel: ObservableObject {
     @Published var showingNewItemView = false
     
-    init() {}
+    private let userId: String
     
+    init(userId: String) {
+        self.userId = userId
+    }
+    
+    /// Delete to do list item
+    /// - Parameter id: item id to delete
     func delete(id: String) {
+        let db = Firestore.firestore()
         
+        db.collection("users")
+            .document(userId)
+            .collection("todos")
+            .document(id)
+            .delete()
     }
 }

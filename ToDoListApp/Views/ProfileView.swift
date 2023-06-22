6d319b6b2f86990b6f4ee86e1fc3115ea28179e1
sirzes02 <sirzes02@gmail.com>
2023-06-21 20:42:01 -0500

    implement request for profile view

--- ToDoListApp/Views/ProfileView.swift
+++ ToDoListApp/Views/ProfileView.swift
@@ -9,14 +9,65 @@ import SwiftUI
 
 struct ProfileView: View {
     @StateObject var viewModel = ProfileViewViewModel()
-
+    
     var body: some View {
         NavigationView {
             VStack {
-                
+                if let user = viewModel.user {
+                    profile(user: user)
+                } else {
+                    Text("Loading Profile...")
+                }
             }
             .navigationTitle("Profile")
         }
+        .onAppear {
+            viewModel.fetchUser()
+        }
+    }
+    
+    @ViewBuilder
+    func profile(user: User) -> some View {
+        Image(systemName: "person.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .foregroundColor(Color.blue)
+            .frame(width: 125, height: 125)
+            .padding()
+        
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Name: ")
+                    .bold()
+                
+                Text(user.name)
+            }
+            .padding()
+            
+            HStack {
+                Text("Email: ")
+                    .bold()
+                
+                Text(user.email)
+            }
+            .padding()
+            
+            HStack {
+                Text("Member since: ")
+                    .bold()
+                
+                Text(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))
+            }
+            .padding()
+        }
+        
+        Button("Log out") {
+            viewModel.logOut()
+        }
+        .tint(.red)
+        .padding()
+        
+        Spacer()
     }
 }
 
